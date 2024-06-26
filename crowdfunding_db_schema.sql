872f52a34b1f8d7245a1e1b727495f6309899110
sacidebelair <166092856+sacidebelair@users.noreply.github.com>
2024-06-26 19:40:22 -0400

    adding final file

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,44 @@
+-- Create the category table
+CREATE TABLE "category" (
+    "category_id" VARCHAR PRIMARY KEY,
+    "category_name" VARCHAR NOT NULL
+);
+
+SELECT category. emp_no, last_name, first_name, sex, salary
+FROM category
+
+	
+-- Create the subcategory table
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR PRIMARY KEY,
+    "subcategory_name" VARCHAR NOT NULL
+);
+
+-- Create the campaign table
+CREATE TABLE "campaign" (
+    "cf_id" int PRIMARY KEY,
+    "contact_id" int,
+    "company_name" VARCHAR,
+    "description" TEXT,
+    "goal" FLOAT,
+    "pledged" FLOAT,
+    "outcome" VARCHAR,
+    "backers_count" INT,
+    "country" VARCHAR,
+    "currency" VARCHAR,
+    "launch_date" DATE,
+    "end_date" DATE,
+    "category_id" VARCHAR REFERENCES category(category_id),
+    "subcategory_id" VARCHAR REFERENCES subcategory(subcategory_id)
+);
+
+
+
+
+-- Create the contacts table
+CREATE TABLE "contacts" (
+    "contact_id" int PRIMARY KEY,
+    "first_name" VARCHAR,
+    "last_name" VARCHAR,
+    "email" VARCHAR
+);
